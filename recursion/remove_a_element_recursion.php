623d1b349b9ecdf3c9b089db3d3d2cd378acba38
Sahil <sahilsarpal@gmail.com>
2018-10-25 17:25:29 +0530

    c code added

---
+++ recursion/remove_a_element_recursion.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class RemoveAElement {
+
+	public function remake($string, $position)
+	{
+		str_
+		return $this->remake($string, ++$position);
+	}
+}
+
+
+$removeAElement = new RemoveAElement;
+
+$anss = $removeAElement->remake("abcded", 0);
+var_dump($anss);
